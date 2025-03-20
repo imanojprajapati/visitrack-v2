1ac4e30d588c1a7198e864750559bed8d07fffa2
imanojprajapati <mnprajapati007@gmail.com>
2025-03-20 14:15:28 +0530

    Pricing Page

--- src/app/pricing/page.jsx
+++ src/app/pricing/page.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
-const Pricing = () => {
-  return (
-    <div>
-      Pricing
-    </div>
-  )
+import AutoSlider from "@/src/app/components/common/auto-slider";
+import BreadCrumb from "@/src/app/components/common/Breadcrumb";
+import Faq from "@/src/app/components/home/faq";
+import Testimonials from "@/src/app/components/home/testimonials";
+import PricePlan from "@/src/app/components/price/price-plan";
+export const metadata = {
+	title: "Pricing || Visitrack",
+	description: "",
+};
+function Pricing() {
+	return (
+		<>
+			<BreadCrumb title="Pricing" />
+			<PricePlan />
+			<AutoSlider />
+			<Testimonials />
+			<Faq />
+		</>
+	);
 }
 
-export default Pricing
+export default Pricing;
